477519b08828d8bfff5725d2f41b8f9a9f3ead04
hareshbhai <haresh.prajapati@outlook.com>
2020-04-12 22:05:20 +0530

    add Renderer2 for styling for custom directive

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -10,6 +10,6 @@ export class BasicHighlightDirective implements OnInit{
     }
 
     ngOnInit(){
-        this.elementRef.nativeElement.style.backgroundColor = 'blue';
+        this.elementRef.nativeElement.style.backgroundColor = 'orange';
     }
 }
